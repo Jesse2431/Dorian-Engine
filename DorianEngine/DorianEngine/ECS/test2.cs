146068c2d5747525777e9d63d5ad649925e574ee
Jesse <jespower2007@gmail.com>
2023-12-18 20:22:44 +0100

    ECS?

---
+++ DorianEngine/DorianEngine/ECS/test2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DorianEngine.ECS
+{
+    internal class test2
+    {
+    }
+}
---
+++ DorianEngine/DorianEngine/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new DorianEngine.Game1();
+game.Run();
